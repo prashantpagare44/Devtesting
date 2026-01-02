7a0ff1d980d66d4429e4e9e137ed4f5411578db9
prashantpagare44 <prashantpagarejd@gmail.com>
2026-01-02 12:07:39 +0530

    changes for team lead fiel

--- src/component/Teamlead.jsx
+++ src/component/Teamlead.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 function Teamlead() {
   return (
-    <div>Teamlead</div>
-  )
+    <div style={{ padding: '20px' }}>
+      <h1>Team Lead Dashboard</h1>
+
+      <p>
+        This is a testing component for the <strong>Team Lead</strong> page.
+      </p>
+
+      <ul>
+        <li>View team members</li>
+        <li>Assign tasks</li>
+        <li>Track progress</li>
+      </ul>
+
+      <button onClick={() => alert('Testing button clicked')}>
+        Test Action
+      </button>
+    </div>
+  );
 }
 
-export default Teamlead
+export default Teamlead;
